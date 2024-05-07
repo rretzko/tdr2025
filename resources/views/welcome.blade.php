77bc4b84f07619cf91d5d2c928871a7da7f37eb1
MFR Holdings <rick@mfrholdings.com>
2024-05-07 13:47:30 -0400

    Removed duplicate <title> tag.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title><title>{{ config('app.name', 'TDR.2025') }}</title></title>
+    <title>{{ config('app.name', 'TDR.2025') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
