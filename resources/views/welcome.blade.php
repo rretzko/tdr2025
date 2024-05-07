7bb9919588c2eab42ea4eb491e7f8df1665a4e50
MFR Holdings <rick@mfrholdings.com>
2024-05-07 12:31:53 -0400

    Updated <title> in welcome.blade.php.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title><title>{{ config('app.name', 'TDR.2025') }}</title></title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
