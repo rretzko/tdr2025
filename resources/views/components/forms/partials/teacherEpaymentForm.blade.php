eb4bea6f2d67c6f1495e09d8801ec0704c719515
MFRHoldings <rick@mfrholdings.com>
2024-10-08 16:28:50 -0400

    Replaced dummy amount in amountDue with calculation. WIP: Add functionality to schedule schools and then order candidates by school arrival time. WIP: add student assignment with asset assignment to ensembles domain. WIP: Migrate from log to ses mailing. WIP: Build link to process verification action when link is clicked. WIP: testing for links to subordinate pages. WIP: Policy for director viewing of student pages to only allow teacher to view student. WIP: Policy for director viewing of student pages to prohibit non-verified email of director to view student entered information.

--- resources/views/components/forms/partials/teacherEpaymentForm.blade.php
+++ resources/views/components/forms/partials/teacherEpaymentForm.blade.php
@@ -22,110 +22,11 @@
     @else
         @if($ePaymentVendor !== 'none')
             @if($ePaymentVendor === 'paypal')
-                <form
-                    action="@if($sandbox) https://www.sandbox.paypal.com/cgi-bin/webscr @else https://www.paypal.com/cgi-bin/webscr @endif "
-                    class="mt-4 p-2 shadow-lg"
-                    method="post"
-                    target="_blank"
-                >
-
-                    <div class="flex flex-row space-x-2">
-                        <label>Click the PayPal button to pay the Amount Due:</label>
-                        <div>${{ number_format($amountDue, 2) }}</div>
-                    </div>
-
-            <!-- Identify your business so that you can collect the payments. -->
-            <input type="hidden" name="business" value="{{ $sandbox ? $sandboxId : $epaymentId }}">
-            <input type="hidden" name="notify_url"
-                   value="https://thedirectorsroom.com/epaymentUpdate">
-            <input type="hidden" name="custom" value="{{ $customProperties }}">
-            <!-- Specify a subscribe button -->
-            <input type="hidden" name="cmd" value="_xclick">
-            <!-- Identify the registrant -->
-            <input type="hidden" name="item_name" value="{{ $versionShortName }}">
-                    <input type="hidden" name="item_number" value="{{ $versionId }}">
-                    <input type="hidden" name="on0" value="{{ $teacherName }}">
-                    <input type="hidden" name="email" value="{{ $sandbox ? $sandboxPersonalEmail : $email }}">
-                    <input type="hidden" name="currency_code" value="USD">
-                    <input type="hidden" name="amount" value="{{ $amountDue }}">
-                    <!-- display the payment button -->
-                    <input class="rounded-full" type="image" name="submit"
-                           src="{{ Storage::disk('s3')->url('pp.png') }}"
-                           alt="PayPal button">
-
-                </form>
+                @include('components.forms.partials.epayments.paypal')
             @endif
             @if($ePaymentVendor === 'square')
                 {{-- SQUARE BUTTON CODE --}}
-                <div>
-                    <div>
-                        <div style="
-overflow: auto;
-display: flex;
-flex-direction: column;
-justify-content: flex-end;
-align-items: center;
-width: 100%;
-background: #FFFFFF;
-border: 1px solid rgba(0, 0, 0, 0.1);
-box-shadow: -2px 10px 5px rgba(0, 0, 0, 0);
-border-radius: 10px;
-font-family: SQ Market, SQ Market, Helvetica, Arial, sans-serif;
-">
-                            <div style="padding: 20px; display: flex; flex-direction: column;">
-                                <label>Please pay ${{ number_format($amountDue, 2) }} via Square.com</label>
-                                <a target="_blank" data-url="https://square.link/u/6NAX5esZ?src=embd"
-                                   href="https://square.link/u/6NAX5esZ?src=embed" style="
-display: inline-block;
-font-size: 18px;
-line-height: 48px;
-height: 48px;
-color: #ffffff;
-min-width: 212px;
-background-color: #006aff;
-text-align: center;
-box-shadow: 0 0 0 1px rgba(0,0,0,.1) inset;
-border-radius: 6px;
-">Pay now</a>
-                            </div>
-                        </div>
-
-                        <script>
-                            function showCheckoutWindow(e) {
-                                e.preventDefault();
-
-                                const url = document.getElementById('embedded-checkout-modal-checkout-button').getAttribute('data-url');
-                                const title = 'Square Payment Links';
-
-                                // Some platforms embed in an iframe, so we want to top window to calculate sizes correctly
-                                const topWindow = window.top ? window.top : window;
-
-                                // Fixes dual-screen position                                Most browsers          Firefox
-                                const dualScreenLeft = topWindow.screenLeft !== undefined ? topWindow.screenLeft : topWindow.screenX;
-                                const dualScreenTop = topWindow.screenTop !== undefined ? topWindow.screenTop : topWindow.screenY;
-
-                                const width = topWindow.innerWidth ? topWindow.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-                                const height = topWindow.innerHeight ? topWindow.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
-
-                                const h = height * .75;
-                                const w = 500;
-
-                                const systemZoom = width / topWindow.screen.availWidth;
-                                const left = (width - w) / 2 / systemZoom + dualScreenLeft;
-                                const top = (height - h) / 2 / systemZoom + dualScreenTop;
-                                const newWindow = window.open(url, title, `scrollbars=yes, width=${w / systemZoom}, height=${h / systemZoom}, top=${top}, left=${left}`);
-
-                                if (window.focus) newWindow.focus();
-                            }
-
-                            // This overrides the default checkout button click handler to show the embed modal
-                            // instead of opening a new tab with the given link url
-                            document.getElementById('embedded-checkout-modal-checkout-button').addEventListener('click', function (e) {
-                                showCheckoutWindow(e);
-                            });
-                        </script>
-                    </div>
-                </div>
+                @include('components.forms.partials.epayments.square')
             @endif
         @endif
     @endif
