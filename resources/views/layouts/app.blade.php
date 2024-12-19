6c249478efbfe4ad16d29db2758291729a4ede15
MFRHoldings <rick@mfrholdings.com>
2024-12-19 14:59:33 -0500

    Adds <meta http-equiv="refresh" content="{{ config('session.lifetime') * 60 }}"> to app and guest layouts to avoid expired forms - resolves #6. 2024.12.19.02 WIP: Add functionality to schedule schools and then order candidates by school arrival time. WIP: add student assignment with asset assignment to ensembles domain. WIP: Migrate from log to ses mailing. WIP: Build link to process verification action when link is clicked. WIP: testing for links to subordinate pages. WIP: Policy for director viewing of student pages to only allow teacher to view student. WIP: Policy for director viewing of student pages to prohibit non-verified email of director to view student entered information.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="refresh" content="{{ config('session.lifetime') * 60 }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
