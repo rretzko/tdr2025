f91f517e567cb8c9bd8cdbd7565e8f231399734b
MFRHoldings <rick@mfrholdings.com>
2025-02-15 12:51:32 -0500

    Adding unversioned files. 2025.02.15.01 WIP: Add functionality to schedule schools and then order candidates by school arrival time. WIP: add student assignment with asset assignment to ensembles domain. Build link to process verification action when link is clicked. WIP: testing for links to subordinate pages. WIP: Policy for director viewing of student pages to only allow teacher to view student. WIP: Policy for director viewing of student pages to prohibit non-verified email of director to view student entered information.

---
+++ database/factories/Libraries/Items/ItemFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Libraries\Items;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Libraries\Items\Item>
+ */
+class ItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
