2e1052758dd9b379f42a3e9cc96450f31c5efa43
MFRHoldings <rick@mfrholdings.com>
2025-03-19 10:25:23 -0400

    Fix sql for payments table in StudentPaymentsService.php. 2025.03.19 WIP: Add functionality to schedule schools and then order candidates by school arrival time. WIP: add student assignment with asset assignment to ensembles domain. Build link to process verification action when link is clicked. WIP: testing for links to subordinate pages. WIP: Policy for director viewing of student pages to only allow teacher to view student. WIP: Policy for director viewing of student pages to prohibit non-verified email of director to view student entered information.

--- resources/views/components/site-footer.blade.php
+++ resources/views/components/site-footer.blade.php
@@ -37,7 +37,7 @@
             powered by:
             <a href="https://mfrholdings.com">
                 MFR Holdings, LLC
-                <span style="margin-left: 0.5rem; font-size: smaller;"> v.2025.03.18.01</span>
+                <span style="margin-left: 0.5rem; font-size: smaller;"> v.2025.03.19</span>
             </a>
 
         </div>
