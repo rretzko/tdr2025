2bf0fb8d03b78c7c5939abb03f4edd7b2275b485
MFRHoldings <rick@mfrholdings.com>
2025-08-04 14:52:34 -0400

    WIP: Add placeholder pages for add/edit of library digital, cd, dvd, cassette, and vinyl items. 2025.08.04-1 WIP: Add functionality to schedule schools and then order candidates by school arrival time. WIP: add student assignment with asset assignment to ensembles domain. Build link to process verification action when link is clicked. WIP: testing for links to subordinate pages. WIP: Policy for director viewing of student pages to only allow teacher to view student. WIP: Policy for director viewing of student pages to prohibit non-verified email of director to view student entered information.

---
+++ resources/views/components/forms/libraries/itemTypes/vinylForm.blade.php
@@ -0,0 +1,28 @@
+<div>
+    @php($moduleName="vinyl items")
+    @include('components.underConstructions.underConstructionApp')
+    {{--    <x-underConstructions.underConstructionApp moduleName="digital library items" libraryId="{{ $form->libraryId }}"/>--}}
+    {{-- TITLE --}}
+    {{--    @include('components.forms.elements.livewire.libraryItem.title')--}}
+
+    {{-- VOICING --}}
+    {{--    @include('components.forms.elements.livewire.libraryItem.voicings')--}}
+
+    {{-- COUNT & PRICE --}}
+    {{--    @include('components.forms.elements.livewire.libraryItem.count')--}}
+
+    {{-- ARTISTS --}}
+    {{--    @include('components.forms.elements.livewire.libraryItem.artistsBlock')--}}
+
+    {{-- TAGS --}}
+    {{--    @include('components.forms.elements.livewire.libraryItem.tags')--}}
+
+    {{-- COMMENTS AND RATING --}}
+    @if(auth()->user()->isTeacher())
+        {{--        @include('components.forms.elements.livewire.libraryItem.commentsRatings')--}}
+    @endif
+
+    {{-- LOCATION --}}
+    {{--    @include('components.forms.elements.livewire.libraryItem.location')--}}
+
+</div>
---
+++ resources/views/components/underConstructions/underConstructionApp.blade.php
@@ -0,0 +1,20 @@
+<div class="text-center text-gray-900 p-6 max-w-md mx-auto">
+    <!-- Emoji Graphic -->
+    <div class="text-7xl mb-4 animate-bounce">
+        📚🚧👷‍♂️
+    </div>
+
+    <!-- Title -->
+    <h1 class="text-3xl font-bold mb-2">{{ ucwords($moduleName) }} Module Is Under Construction</h1>
+
+    <!-- Tagline -->
+    <p class="text-gray-800 dark:text-gray-200 mb-6">
+        We’re still shelving the books, dusting the archives, and trying to alphabetize everything correctly...
+    </p>
+
+    <!-- Back Button -->
+    <a href="/library/{{ $libraryId }}/items"
+       class="inline-block bg-blue-600 text-white px-5 py-2 rounded hover:bg-blue-700 dark:bg-yellow-500 dark:hover:bg-yellow-400 transition">
+        ⬅ Back to Items Table
+    </a>
+</div>
