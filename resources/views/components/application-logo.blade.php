8c1e61a65d60b9ae0c54a57f0b0c9c90fb3c821b
MFRHoldings <rick@mfrholdings.com>
2025-08-18 15:07:51 -0400

    Updt: Update logo 2025.08.18-5 WIP: Add functionality to schedule schools and then order candidates by school arrival time. WIP: add student assignment with asset assignment to ensembles domain. Build link to process verification action when link is clicked. WIP: testing for links to subordinate pages. WIP: Policy for director viewing of student pages to only allow teacher to view student. WIP: Policy for director viewing of student pages to prohibit non-verified email of director to view student entered information.

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,8 +1,13 @@
 @props([
     'width' => null,
 ])
-<img src="https://auditionsuite-production.s3.amazonaws.com/thumbnails/swirlGclef_thumb.png"
+{{--<img src="https://auditionsuite-production.s3.amazonaws.com/thumbnails/swirlGclef_thumb.png"--}}
+{{--     @if($width) width="{{$width}}" @endif--}}
+{{--     alt="g-clef thumbnail image"--}}
+{{--/>--}}
+
+<img src="{{ Storage::disk('s3')->url('backgrounds/tdr-monogram-raisedT.svg') }}"
      @if($width) width="{{$width}}" @endif
-     alt="g-clef thumbnail image"
+     alt="TDR monogram"
 />
 
