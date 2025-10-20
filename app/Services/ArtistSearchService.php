29f8d2b132b20f806a430e983c83c92a57d76887
rretzko <rick@mfrholdings.com>
2025-10-20 14:46:30 -0400

    Updt: Change ArtistSearchService.php to return name (as well as alpha name), ordered by first_name, last_name. 2025-10-20

--- app/Services/ArtistSearchService.php
+++ app/Services/ArtistSearchService.php
@@ -15,20 +15,27 @@ class ArtistSearchService
         return self::$results;
     }
 
+    /**
+     * artists are return ordered by last_name and then first name
+     * This ordering returns all multi-artist artists( ex. John Lennon and Paul McCartney)
+     * first (first_name on these artists is blank), in last_name order
+     * and then all single artists (ex. Ringo Starr) in first_name, last_name order
+    */
     private static function init(string $searchFor, string $artistType): void
     {
         $search = '%'.$searchFor.'%';
 
         $artists = DB::table('artists')
             ->where('artist_name', 'LIKE', $search)
-            ->orderBy('last_name', 'asc')
             ->orderBy('first_name', 'asc')
+            ->orderBy('last_name', 'asc')
             ->get();
 
         foreach ($artists as $artist) {
 
             self::$results[] = [
-                'name' => $artist->alpha_name,
+                'alpha' => $artist->alpha_name,
+                'name' => $artist->artist_name,
                 'type' => $artistType,
                 'id' => $artist->id,
             ];
